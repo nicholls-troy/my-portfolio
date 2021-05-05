e570fd0158f071e71184705375503dff2ae7b780
trn2 <trn2@queensu.ca>
2021-05-05 09:34:16 -0400

    change background picture on about page and photo size

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ export default function About() {
             <img src={mountains} alt="Mountains and Lake" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-gray-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-16 h-32 lg:w-48 lg:h-64 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
                         <h1 className="cursive text-6xl text-gray-300 mb-4">
                             Hey there. I'm{" "}
